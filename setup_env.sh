b9adb67fe1a152bb02b4f32da241540d104f0fe6
Miro Kropacek <miro.kropacek@gmail.com>
2017-05-06 23:38:02 +1000

    Make commit id clickable

--- setup_env.sh
+++ setup_env.sh
@@ -10,7 +10,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
 then
     export PUBLISH_REPO="git@github.com:${USER}/freemint.github.io.git"
     export PUBLISH_PATH="builds/${PROJECT}/${TRAVIS_BRANCH}"
-    export COMMIT_MESSAGE="[${PROJECT}] [${TRAVIS_BRANCH}] Commit ${TRAVIS_COMMIT}"
+    export COMMIT_MESSAGE="[${PROJECT}] [${TRAVIS_BRANCH}] Commit: https://github.com/${USER}/${PROJECT}/commit/${TRAVIS_COMMIT}"
 else
     export PUBLISH_REPO="git@github.com:${USER}/travis-pr.git"
     export PUBLISH_PATH="builds/${PROJECT}/${TRAVIS_PULL_REQUEST}"
