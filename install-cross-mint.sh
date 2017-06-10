f13f33edcb0d6a188b3ebee7e5daffbe5c1e7e6b
Miro Kropacek <miro.kropacek@gmail.com>
2017-06-11 09:16:50 +1000

    Auto confirmation for adding ppa

--- install-cross-mint.sh
+++ install-cross-mint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Usage: install-cross-mint.sh <package name> [<package name>...]
 
-sudo add-apt-repository ppa:vriviere/ppa
+sudo add-apt-repository ppa:vriviere/ppa -y
 sudo apt-get update
 sudo apt-get install -y $*
